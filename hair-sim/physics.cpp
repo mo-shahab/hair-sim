6b55e251fe272641f3616548dc3ad1231fb0b6c0
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2023-05-23 20:00:15 +0530

    fix: hair strand mem allocation

--- hair-sim/physics.cpp
+++ hair-sim/physics.cpp
@@ -7,35 +7,38 @@ HairStrand createHairStrand(int numVertices, int numSprings) {
     strand.numSprings = numSprings;
 
     // Allocate memory for vertices and springs
-    strand.vertices = (Vertex*)malloc(numVertices * sizeof(Vertex));
-    strand.springs = (Spring*)malloc(numSprings * sizeof(Spring));
+    //strand.vertices = new Vertex[numVertices];
+    strand.springs = new Spring[numSprings];
 
     // Initialize vertices and springs
 
     return strand;
 }
 
+
 void destroyHairStrand(HairStrand& strand) {
     // Free memory
-    free(strand.vertices);
-    free(strand.springs);
+    //delete[] strand.numVertices;
+    delete[] strand.springs;
 }
 
-void applyForces(std::vector<Vertex>& hairVertices, float gravity, const glm::vec3& windDirection, float windStrength) {
+
+
+void applyForces(Vertex* hairVertices, int numVertices, float gravity, const glm::vec3& windDirection, float windStrength) {
     // Apply gravitational force to each hair vertex
-    for (auto& vertex : hairVertices) {
-        vertex.position.y -= gravity; // Assuming y-axis is the vertical axis
+    for (int i = 0; i < numVertices; ++i) {
+        hairVertices[i].position.y -= gravity; // Assuming y-axis is the vertical axis
     }
 
     // Apply wind force to each hair vertex based on wind direction and strength
-    for (auto& vertex : hairVertices) {
-        vertex.position += windDirection * windStrength;
+    for (int i = 0; i < numVertices; ++i) {
+        hairVertices[i].position += windDirection * windStrength;
     }
 }
 
-void calculateSpringForces(const std::vector<Vertex>& hairVertices, float springStiffness, float restLength, std::vector<glm::vec3>& springForces) {
+void calculateSpringForces(const Vertex* hairVertices, int numVertices, float springStiffness, float restLength, std::vector<glm::vec3>& springForces) {
     // Calculate spring forces between adjacent hair vertices
-    for (size_t i = 1; i < hairVertices.size(); ++i) {
+    for (int i = 1; i < numVertices; ++i) {
         const glm::vec3& currentPos = hairVertices[i].position;
         const glm::vec3& previousPos = hairVertices[i - 1].position;
         glm::vec3 springForce = springStiffness * (currentPos - previousPos) * (glm::length(currentPos - previousPos) - restLength);
@@ -43,34 +46,34 @@ void calculateSpringForces(const std::vector<Vertex>& hairVertices, float spring
     }
 }
 
-void applyDampingForces(std::vector<Vertex>& hairVertices, float dampingCoefficient) {
+void applyDampingForces(Vertex* hairVertices, int numVertices, float dampingCoefficient) {
     // Apply damping forces to each hair vertex based on its velocity
-    for (auto& vertex : hairVertices) {
-        vertex.position -= dampingCoefficient * vertex.position;
+    for (int i = 0; i < numVertices; ++i) {
+        hairVertices[i].position -= dampingCoefficient * hairVertices[i].position;
     }
 }
 
-void integrate(std::vector<Vertex>& hairVertices, float timeStep) {
+void integrate(Vertex* hairVertices, int numVertices, float timeStep) {
     // Perform numerical integration (e.g., Euler integration) to update positions and velocities
-    for (auto& vertex : hairVertices) {
+    for (int i = 0; i < numVertices; ++i) {
         // Update position using velocity and time step
-        vertex.position += vertex.velocity * timeStep;
+        hairVertices[i].position += hairVertices[i].velocity * timeStep;
 
         // Update velocity (e.g., by applying external forces or constraints)
         // ...
     }
 }
 
-void updateHair(std::vector<Vertex>& hairVertices, float gravity, const glm::vec3& windDirection, float windStrength, float springStiffness, float restLength, float dampingCoefficient, float timeStep) {
+void updateHair(Vertex* hairVertices, int numVertices, float gravity, const glm::vec3& windDirection, float windStrength, float springStiffness, float restLength, float dampingCoefficient, float timeStep) {
     // Apply external forces (e.g., gravity, wind)
-    applyForces(hairVertices, gravity, windDirection, windStrength);
+    applyForces(hairVertices, numVertices, gravity, windDirection, windStrength);
 
     // Calculate spring forces
     std::vector<glm::vec3> springForces;
-    calculateSpringForces(hairVertices, springStiffness, restLength, springForces);
+    calculateSpringForces(hairVertices, numVertices, springStiffness, restLength, springForces);
 
     // Apply spring forces
-    for (size_t i = 1; i < hairVertices.size(); ++i) {
+    for (int i = 1; i < numVertices; ++i) {
         glm::vec3& currentPos = hairVertices[i].position;
         glm::vec3& previousPos = hairVertices[i - 1].position;
         glm::vec3& currentVelocity = hairVertices[i].velocity;
